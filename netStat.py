3e112c83d75a034a84858c3246f60c16ef805232
AstroWLAN <dario2.crippa@mail.polimi.it>
2025-03-03 10:39:45 +0100

    Removes cython, tshark and other useless stuff

--- netStat.py
+++ netStat.py
@@ -1,11 +1,5 @@
 import numpy as np
-## Prep AfterImage cython package
-import os
-import subprocess
-import pyximport
-pyximport.install()
 import AfterImage as af
-#import AfterImage_NDSS as af
 
 #
 # MIT License
