19e4a240fb6d06ba10bb48dd48c92431b11d29d3
AstroWLAN <dario2.crippa@mail.polimi.it>
2025-03-06 18:10:49 +0100

    Adds support for precision visualization

--- ResearchTools/tools.py
+++ ResearchTools/tools.py
@@ -152,7 +152,7 @@ def performance_analysis (predictions, flowIDs, true_labels, rates = None):
     
     # Sets the default rates if none are provided
     if rates is None:
-        rates = [0.01, 0.1, 0.25, 0.5, 0.75, 1]
+        rates = [0.001, 0.01, 0.1, 0.25, 0.5, 0.75, 1]
     
     print(f"\033[90mCollecting the performance of the architectures\033[0m")
     for rate in rates:
@@ -171,20 +171,29 @@ def performance_analysis (predictions, flowIDs, true_labels, rates = None):
         wasps_precision_results.append(wasps_precision)
         wasps_f1_score_results.append(wasps_f1)
         
-    # Generates the recall plot
-    plt.figure(figsize=(10, 6))
     # Calculate inverse for each rate
     inverse_rates = [1/rate for rate in rates]
     
-    # Plot both naive and wasps results with the same line style but different colors
+    # Generates the recall plot
+    plt.figure(figsize=(10, 6))
     plt.plot(inverse_rates, naive_recall_results, marker='o', linestyle='-', linewidth=2, color='blue', label='Naive Sampling')
     plt.plot(inverse_rates, wasps_recall_results, marker='o', linestyle='-', linewidth=2, color='green', label='Wasps Detection')
-    
     plt.xlabel('1/R')
     plt.ylabel('Recall')
     plt.grid(True, alpha=0.3, linestyle='--', color='gray')
     plt.legend()
-    
     plt.tight_layout()
     plt.savefig('recall_vs_inverse_sampling_rate.png')
+    plt.show(block=False)
+    
+    # Generates the precision plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(inverse_rates, naive_precision_results, marker='o', linestyle='-', linewidth=2, color='blue', label='Naive Sampling')
+    plt.plot(inverse_rates, wasps_precision_results, marker='o', linestyle='-', linewidth=2, color='green', label='Wasps Detection')
+    plt.xlabel('1/R')
+    plt.ylabel('Precision')
+    plt.grid(True, alpha=0.3, linestyle='--', color='gray')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('precision_vs_inverse_sampling_rate.png')
     plt.show(block=False)
